171e43aa30b9d855e187fbd8545a3097af9185f2
NotYourSyn <60495598+NotYourSyn@users.noreply.github.com>
2020-10-14 20:14:43 -0400

    built reducer -todo-list

---
+++ todo/src/reducers/TodoReducer.js
@@ -0,0 +1,41 @@
+export const initialState = [{
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  },
+  {
+      item: 'Get my homework done ',
+      completed: false,
+      id: 2390472389
+  }
+]
+
+
+export const todoReducer = (state, action) => {
+
+
+switch(action.type) {
+    case 'ADD_TODO':
+        console.log(state, action)
+        return [...state, { item: action.payload, id:Date.now(), completed: false }]
+
+    case 'MARK_COMPLETE':
+        state.map(item => {
+            if (item.id === action.payload){
+            return item.completed = !item.completed;
+            }
+        })
+        return [...state]
+
+    case 'CLEAR_COMPLETE':
+        
+        const newArr = state.filter(item => {
+           return item.completed !== true
+        })
+       
+        return [...newArr]
+    
+    default:
+        return state;
+}
+}
